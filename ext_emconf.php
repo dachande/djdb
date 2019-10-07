48684c74447fab973bad39d343a869a096fda8c1
Daniel Schultheis <d.schultheis@reizwerk.com>
2019-10-07 12:48:21 +0200

    [TASK] Add extension configuration file

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,1 +1,44 @@
 <?php
+
+/**
+ * Copyright (C) 2019  Daniel Schultheis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+$EM_CONF['djdb'] = [
+    'title' => 'DJ Set database extension for TYPO3',
+    'description' => '',
+    'version' => '0.1.0',
+    'state' => 'alpha',
+    'category' => 'plugin',
+    'author' => 'Daniel Schultheis',
+    'author_email' => 'd.schultheis@kabel-salat.net',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.2.0-0.0.0',
+            'typo3' => '9.5.0-9.5.99',
+            'vhs' => '5.2.0-0.0.0',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'clearCacheOnLoad' => true,
+    'autoload' => [
+        'psr-4' => [
+            'Dachande\\Djdb\\' => 'Classes',
+        ],
+    ],
+];
