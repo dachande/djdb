fd0cd320af75bdd751bd4c5e6131de8b59e370e8
Daniel Schultheis <d.schultheis@reizwerk.com>
2019-10-07 14:26:16 +0200

    [FEATURE] Add Set Controller with empty list and show actions

---
+++ Classes/Controller/SetController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Dachande\Djdb\Controller;
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class SetController extends ActionController
+{
+    public function listAction()
+    {
+    }
+
+    public function showAction()
+    {
+    }
+}
