2610fa2b6d4b9c0270ef4bb2ff81754c648cc60a
Daniel Schultheis <d.schultheis@reizwerk.com>
2019-10-07 14:30:26 +0200

    [BUGFIX] Fix track table schema

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,6 +8,6 @@ CREATE TABLE tx_djdb_domain_model_set (
 #
 # Table structure for table 'tx_djdb_domain_model_track'
 #
-CREATE TABLE tx_djdb_domain_model_set (
+CREATE TABLE tx_djdb_domain_model_track (
   title tinytext
 );
