19b683c2a72930ecd71fa378e80037191c50181a
Daniel Schultheis <d.schultheis@reizwerk.com>
2019-10-09 12:01:53 +0200

    [TASK] Add copyright notices

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,20 +21,4 @@
 defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
-    $models = [
-        'download',
-        'genre',
-        'recording',
-        'set',
-        'track',
-    ];
-
-    // Add context sensitive help for each model
-    foreach ($models as $table) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_djdb_domain_model_' . $table);
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-            'tx_djdb_domain_model_' . $table,
-            'EXT:djdb/Resources/Private/Language/locallang_csh_' . $table . '.xlf'
-        );
-    }
 });
